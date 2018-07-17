8a324795f7c36950747f56195f6ed02784fad2df
xwp <xwp_fullstack@163.com>
2018-07-17 17:02:31 +0800

    立项

---
+++ users/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+
+from django.shortcuts import render
+
+
+# Create your views here.
+
+
+def index(request):
+    """访问首页的视图"""
+    return HttpResponse("<h2>hello django</h2>")
